f175af70d8dd83239ae3fd89f0ac53a34fd7634c
Kavindu Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2022-06-19 11:06:16 +0530

    Update flip.py
    added some extra features

--- flip.py
+++ flip.py
@@ -1,20 +1,40 @@
 import tkinter as tk
 import random
 import time
+from turtle import color
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+import matplotlib.pyplot as plt
+
+
+root = tk.Tk()
+root.title("COIN FLIPPER")
+
+root.resizable(0,0)
+root.geometry('500x550')
+frame = tk.Frame(root)
+frame.pack()
+
 
 
 choices = ['Head', 'Tail']
 
-flips =[]
+
 
 def plot():
     pass
 
-def start(event):
+def start():
+    flips =[]
+   
     flip_range = int(entry.get())
     for x in range(flip_range):
         result = random.choice(choices)
         flips.append(result)
+
+        
 
         
 
@@ -23,17 +43,39 @@ def start(event):
 
         heads['text'] = "Head Percentage is " + str(head_percentage)+"%"
         tails['text'] = "Tail Percentage is " + str(tail_percentage) +"%" 
+
+
+        data = {
+            'head': head_percentage,
+            'tail': tail_percentage
+        }
+        
+        titles = list(data.keys())
+        probabilities = list(data.values())
+
+        fig = plt.figure(figsize=(5,5))
+        plt.bar(titles,probabilities,color='green',width=0.4)
+        plt.title("turn: "+str(x+1))
+
+        canvas = FigureCanvasTkAgg(fig,frame)
     
-    
+        canvas.get_tk_widget().grid(row=5,columnspan=10)
+
+       
+        root.update()
 
     
+    
+    
+def enter_start(event):
+    start()
+    
 
-root = tk.Tk()
-root.title("COIN FLIPPER")
-root.resizable(0,0)
-root.bind('<Return>',start)
-frame = tk.Frame(root)
-frame.pack()
+
+
+
+root.bind('<Return>',enter_start)
+
 
 entry = tk.Entry(frame)
 button = tk.Button(frame,
@@ -46,10 +88,15 @@ heads = tk.Label(frame)
 tails = tk.Label(frame)
 
 
+
+
+
 button.grid(row=1,column=0)
 entry.grid(row=1, column=1)
 
-heads.grid(row=2,column=0,columnspan=2)
-tails.grid(row=3,column=0,columnspan=2)
+heads.grid(row=2,column=0,columnspan=4)
+tails.grid(row=3,column=0,columnspan=4)
+
+
 
 root.mainloop()
